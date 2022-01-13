0ef04fbc0d9b7db92d4a0178a4a07805f60246c1
MariaRiojas <89672217+MariaRiojas@users.noreply.github.com>
2022-01-13 13:03:59 -0500

    Commit

---
+++ index.js
@@ -0,0 +1,11 @@
+console.log("page loaded...")
+function play(element) {
+    element.videopreview("play")
+    var video = document.play("play");
+        vid.muted= true;
+}
+function out(element) {
+    element.videopreview("pause")
+    element.currentTime = 0;
+}
+
